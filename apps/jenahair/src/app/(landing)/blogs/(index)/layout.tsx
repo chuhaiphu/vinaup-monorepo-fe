4965ab5c7c95e6c3b7a9ded83d98be1b9052efbc
doanvinhphu <doanvinhphu321@gmail.com>
2026-04-16 12:23:50 +0700

    Remove unused layout styles. Refactor folder structure for blogs and tours

--- apps/jenahair/src/app/(landing)/blogs/(index)/layout.tsx
+++ apps/jenahair/src/app/(landing)/blogs/(index)/layout.tsx
@@ -1,11 +1,8 @@
 import { Container } from "@mantine/core";
-import classes from './layout.module.scss';
 
 export default function BlogsPageLayout({ children }: { children: React.ReactNode }) {
   return (
-    <Container size={'xl'} mb={'xl'} classNames={{
-      root: classes.blogsPageLayout
-    }}>
+    <Container size={'xl'} mb={'xl'}>
       {children}
     </Container>
   )
--- apps/jenahair/src/app/(landing)/tours/(index)/layout.tsx
+++ apps/jenahair/src/app/(landing)/tours/(index)/layout.tsx
@@ -1,11 +1,8 @@
 import { Container } from "@mantine/core";
-import classes from './layout.module.scss';
 
 export default function ToursPageLayout({ children }: { children: React.ReactNode }) {
   return (
-    <Container size={'xl'} mb={'xl'} classNames={{
-      root: classes.toursPageLayout
-    }}>
+    <Container size={'xl'} mb={'xl'}>
       {children}
     </Container>
   )
